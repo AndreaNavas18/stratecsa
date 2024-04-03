2fc21b6f74f647ed378f33d81ff48844b152e35a
AndreaNavas18 <karennavas333@gmail.com>
2024-04-03 14:15:56 -0500

    cambiosprimermovil

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -2,41 +2,41 @@ import Styles from './header.module.css';
 
 function Header() {
   return (
-    <section className={Styles.sectionHeader}>
-        <div className={Styles.divHeader1}>
-            <img
-                className={Styles.iconBusqueda} 
-                src="/img/lupa.png"/>
+    <section className={`${Styles.sectionHeader} ${Styles.sectionHeaderM}`}>
+        <div className={`${Styles.divHeader1} ${Styles.divHeader1M}`}>
+            {/* <img
+                className={`${Styles.iconBusqueda} ${Styles.iconBusquedaM}`} 
+                src="/img/lupa.png"/> */}
             
             <a 
                 href='/contacto' 
-                className={Styles.buttonContact}>
+                className={`${Styles.buttonContact} ${Styles.buttonContactM}`}>
                     Contactanos
             </a>
-            <div className={Styles.divRedes}>
-                <a className={Styles.cRedes} href="https://www.facebook.com/Stratecsa?locale=es_LA" target="_blank" rel="noopener noreferrer">
+            <div className={`${Styles.divRedes} ${Styles.divRedesM}`}>
+                <a className={`${Styles.cRedes} ${Styles.cRedesM}`} href="https://www.facebook.com/Stratecsa?locale=es_LA" target="_blank" rel="noopener noreferrer">
                     <img
-                        className={Styles.iconSocial}
+                        className={`${Styles.iconSocial} ${Styles.iconSocialM}`}
                         src="/logos/straface.png"/>
                 </a>
-                <a className={Styles.cRedes} href="https://www.instagram.com/stratecsa/" target="_blank" rel="noopener noreferrer">
+                <a className={`${Styles.cRedes} ${Styles.cRedesM}`} href="https://www.instagram.com/stratecsa/" target="_blank" rel="noopener noreferrer">
                     <img
-                        className={Styles.iconSocial}
+                        className={`${Styles.iconSocial} ${Styles.iconSocialM}`}
                         src="/logos/strains.png"/>
                 </a>
-                <a className={Styles.cRedes} href="https://www.linkedin.com/company/stratecsa" target="_blank" rel="noopener noreferrer">
+                <a className={`${Styles.cRedes} ${Styles.cRedesM}`} href="https://www.linkedin.com/company/stratecsa" target="_blank" rel="noopener noreferrer">
                     <img
-                        className={Styles.iconSocial}
+                        className={`${Styles.iconSocial} ${Styles.iconSocialM}`}
                         src="/logos/stralink.png"/>
                 </a>
-                <a className={Styles.cRedes} href="https://wa.link/7kydod" target="_blank" rel="noopener noreferrer">
+                <a className={`${Styles.cRedes} ${Styles.cRedesM}`} href="https://wa.link/7kydod" target="_blank" rel="noopener noreferrer">
                     <img 
-                        className={Styles.iconSocial}
+                        className={`${Styles.iconSocial} ${Styles.iconSocialM}`}
                         src="/logos/strawhats.png"/>
                 </a>
-                <a className={Styles.cRedes} href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+                <a className={`${Styles.cRedes} ${Styles.cRedesM}`} href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
                     <img 
-                        className={Styles.iconSocial}
+                        className={`${Styles.iconSocial} ${Styles.iconSocialM}`}
                         src="/logos/strayou.png"/>
                 </a>
 
--- src/app/components/header2/header2.jsx
+++ src/app/components/header2/header2.jsx
@@ -1,17 +1,27 @@
+"use client";
 import Styles from './header2.module.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Header2() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return(
-        <section className={Styles.sectionNav}>
-            <div className={Styles.container}>
-                <div className={Styles.divIcon}>
-                <a href="/home">
-                    <img
-                        className={Styles.iconStra}
-                        src="/img/logofinal.png"/>
-                </a>
+        <section className={`${Styles.sectionNav} ${Styles.sectionNavM}`}>
+            <div className={`${Styles.container} ${Styles.containerM}`}>
+                <div className={`${Styles.divIcon} ${Styles.divIconM}`}>
+                    <a href="/home">
+                        <img
+                            className={`${Styles.iconStra} ${Styles.iconStraM}`}
+                            src="/img/logofinal.png"/>
+                    </a>
                 </div>
-                <div className={Styles.divNavBar}>
+                <div className={`${Styles.divNavBar} ${Styles.divNavBarM}`}>
                     <nav>
                         <ul className={Styles.ulNav}>
                             <li className={Styles.liNav}>
@@ -32,8 +42,36 @@ function Header2() {
                         </ul>
                     </nav>
                 </div>
-            </div>
+                <div className={`${Styles.navMovil}`}>
+                    <FontAwesomeIcon icon={faUser} className={Styles.iconClient} />
+                    <FontAwesomeIcon icon={faBars} className={Styles.iconFabars} onClick={toggleMenu} />
 
+                </div>
+            </div>
+            {menuOpen && 
+            <div className={Styles.divMenuMovil}>
+                <FontAwesomeIcon icon={faXmark} className={Styles.closeButton} onClick={toggleMenu} />
+
+                <nav className={Styles.navM}>
+                    <ul className={Styles.ulMenuMovil}>
+                        <li className={Styles.liMenuMovil}>
+                            <a href="/home">Inicio</a>
+                        </li>
+                        <li className={Styles.liMenuMovil}>
+                            <a href="/servicios/internet-dedicado">Servicios</a>
+                        </li>
+                        <li className={Styles.liMenuMovil}>
+                            <a href="/nosotros">Acerca de nosotros</a>
+                        </li>
+                        <li className={Styles.liMenuMovil}>
+                            <a href="/cobertura">Cobertura</a>
+                        </li>
+                        <li className={Styles.liMenuMovil}>
+                            <a href="/contacto">Contactenos</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>}
         </section>
     );
 }
