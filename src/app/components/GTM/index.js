a9564c59cbac83a11e94f3c611c81ded407e2e35
AndreaNavas18 <karennavas333@gmail.com>
2024-05-10 09:58:31 -0500

    newtag

--- src/app/components/GTM/index.js
+++ src/app/components/GTM/index.js
@@ -2,7 +2,7 @@ import Script from "next/script";
 
 export const GTMscript = () => (
     <>
-    <Script>{`window.dataLayer = window.dataLayer || [];`}</Script>
+    <Script id="primer-script">{`window.dataLayer = window.dataLayer || [];`}</Script>
     <Script id="google-tag-manager" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
